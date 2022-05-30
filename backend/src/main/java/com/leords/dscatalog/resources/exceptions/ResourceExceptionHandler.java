2e12357edc879a9cf79da5b93b7b21d150089d5b
Leonardo Ramos de Sousa <leoramosdsousa@gmail.com>
2022-05-29 21:38:15 -0300

    Custom validation response

--- backend/src/main/java/com/leords/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/leords/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import com.leords.dscatalog.services.exceptions.DatabaseException;
 import com.leords.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -12,11 +14,10 @@ import java.time.Instant;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
-    
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException exception, HttpServletRequest request) {
         var status = HttpStatus.NOT_FOUND;
-        StandardError error = new StandardError();
+        var error = new StandardError();
         
         error.setTimestamp(Instant.now());
         error.setStatus(status.value());
@@ -30,7 +31,7 @@ public class ResourceExceptionHandler {
     @ExceptionHandler(DatabaseException.class)
     public ResponseEntity<StandardError> databaseIntegrationViolation(DatabaseException exception, HttpServletRequest request) {
         var status = HttpStatus.BAD_REQUEST;
-        StandardError error = new StandardError();
+        var error = new StandardError();
         
         error.setTimestamp(Instant.now());
         error.setStatus(status.value());
@@ -41,4 +42,21 @@ public class ResourceExceptionHandler {
         return ResponseEntity.status(status).body(error);
     }
     
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> argumentValidation(MethodArgumentNotValidException exception, HttpServletRequest request) {
+        var status = HttpStatus.UNPROCESSABLE_ENTITY;
+        var error = new ValidationError();
+        
+        error.setTimestamp(Instant.now());
+        error.setStatus(status.value());
+        error.setError("Validation exception");
+        error.setMessage(exception.getMessage());
+        error.setPath(request.getRequestURI());
+        
+        for (FieldError f : exception.getBindingResult().getFieldErrors()) {
+            error.addError(f.getField(), f.getDefaultMessage());
+        }
+        
+        return ResponseEntity.status(status).body(error);
+    }
 }
