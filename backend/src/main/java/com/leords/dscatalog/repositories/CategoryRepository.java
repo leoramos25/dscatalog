640185ea46de5ae240799dacdda3a609eff6bf06
Leonardo Ramos de Sousa <leonardords@ciandt.com>
2023-01-07 12:24:34 -0300

    clean project

--- backend/src/main/java/com/leords/dscatalog/repositories/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.leords.dscatalog.repositories;
-
-import com.leords.dscatalog.entities.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
